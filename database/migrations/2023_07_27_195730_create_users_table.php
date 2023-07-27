efd82870698624ecf98ef54a7f6250720dcb48ed
shuvo <saifulshuvo2@gmail.com>
2023-07-28 02:42:09 +0600

    student table migration done

--- database/migrations/2023_07_27_195730_create_users_table.php
+++ database/migrations/2023_07_27_195730_create_users_table.php
@@ -13,11 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
             $table->string('email');
             $table->string('password');
             $table->string('role');
             $table->string('remember_token')->nullable();
+
             $table->timestamps();
         });
     }
--- database/migrations/2023_07_27_201304_create_students_table.php
+++ database/migrations/2023_07_27_201304_create_students_table.php
@@ -13,8 +13,15 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
+            $table->string('roll_number')->unique();
+            $table->date('date_of_birth');
+            $table->text('address');
+            $table->string('gurardian_name');
+            $table->string('guardian_contact');
+
             $table->timestamps();
         });
     }
