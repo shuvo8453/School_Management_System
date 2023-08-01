09832adddbb8fe5195827aa81744b7e5e10356f0
shuvo <saifulshuvo2@gmail.com>
2023-08-02 00:56:05 +0600

    install laravel breeze for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,31 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-Route::get('/login', function() {
-    return view('super-admin.pages.signing');
-})->name('login');
-
-
-// for super admin
 Route::get('/', function () {
-    return view('super-admin.pages.dashboard');
+    return view('welcome');
 });
-Route::get('/admin/dashboard', [SuperAdminController::class, 'dashboard'])->name('dashboard');
-Route::get('/admin/profiles', [SuperAdminController::class, 'Profile'])->name('profile');
-Route::get('/admin/signing', [SuperAdminController::class, 'signing'])->name('signing');
-Route::get('/admin/signup', [SuperAdminController::class, 'signup'])->name('signup');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// for admin
-Route::get('/', function () {
-    return view('admin.pages.dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
-Route::get('/profiles', [AdminController::class, 'Profile'])->name('profile');
-Route::get('/signing', [AdminController::class, 'signing'])->name('signing');
-Route::get('/signup', [AdminController::class, 'signup'])->name('signup');
+
+require __DIR__.'/auth.php';
