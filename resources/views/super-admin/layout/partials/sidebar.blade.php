2026ca5bc08fe7b84361bd0404a93f80ffb0740b
shuvo <saifulshuvo2@gmail.com>
2023-08-03 02:29:22 +0600

    make side-bar active

--- resources/views/super-admin/layout/partials/sidebar.blade.php
+++ resources/views/super-admin/layout/partials/sidebar.blade.php
@@ -9,20 +9,20 @@
                 Pages
             </li>
 
-            <li class="sidebar-item active">
+            <li class="sidebar-item {{ (request()->is('superadmin/dashboard')) ? 'active' : '' }}">
                 <a class="sidebar-link" href=" {{ route('superadmin.dashboard') }}  ">
                     <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
                 </a>
             </li>
 
-            <li class="sidebar-item">
+            <li class="sidebar-item {{ (request()->is('superadmin/profiles')) ? 'active' : '' }}">
                 <a class="sidebar-link" href="{{ route('superadmin.profile') }}">
                     <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
                 </a>
             </li>
 
-            <li class="sidebar-item">
-                <a class="sidebar-link" href="{{ route('superadmin.profile') }}">
+            <li class="sidebar-item {{ (request()->is('superadmin/admin')) ? 'active' : '' }}">
+                <a class="sidebar-link" href="{{ route('superadmin.admin') }}">
                     <i class="align-middle" data-feather="user"></i> <span class="align-middle">Admin</span>
                 </a>
             </li>
