be2671bf62324a18f0642c62a61a2117547df6e3
shuvo <saifulshuvo2@gmail.com>
2023-08-08 01:13:02 +0600

    working on profile edit

--- resources/views/super-admin/navbar.blade.php
+++ resources/views/super-admin/navbar.blade.php
@@ -8,7 +8,7 @@
 
 <div class="dropdown-menu dropdown-menu-end">
     <a class="dropdown-item" href="{{ route('superadmin.profile') }}"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
-    <a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings</a>
+    {{-- <a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings</a> --}}
     <div class="dropdown-divider"></div>
     <form action="{{ route('logout') }}" method="post">
         @csrf
